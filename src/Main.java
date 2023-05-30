ae02af6179a0bb0527ef364636a744ba116b8370
azuressu <martina0130@naver.com>
2023-05-30 14:59:48 +0900

    Fourth Commit - Goods.java and Main.java, Menu.java add annotation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-import java.awt.color.ICC_ColorSpace;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        MyMenu myMenu = new MyMenu();
+        MyMenu myMenu = new MyMenu();           // 주문할 수 있는 인스턴스 객체 생성
         try {
             myMenu.mainMenu();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (Exception e) {                 // 예외 처리 필수
+            System.out.println(e.getMessage()); // 예외 관련 메시지 출력
         }
-
     }
 }
